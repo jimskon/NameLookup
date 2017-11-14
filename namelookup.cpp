74a2ccf4751d386dfe8202c606b0696c76aabef1
Jim Skon <skonjp@kenyon.edu>
2017-11-14 09:26:40 -0500

    Minor cleanup

--- namelookup.cpp
+++ namelookup.cpp
@@ -29,29 +29,34 @@
 using namespace std;
 using namespace cgicc; // Needed for AJAX functions.
 
+// Files with the name data
 const string path = "/home/class/SoftDev/namedata/";
 const string lastFile = path+"dist.all.last";
 const string maleFile = path+"dist.male.first";
 const string femaleFile = path+"dist.female.first";
 
 int main() {
+  // Object used for receiving AJAX call (and getting parameters)
   Cgicc cgi;    // Ajax object
-  char *cstr;
+
   // Build the name maps
   NameMap lastMap(lastFile);
   NameMap femaleMap(femaleFile);
   NameMap maleMap(maleFile);
   vector<NameEntry> matchList; // place to stare the matches
   
-  // Create AJAX objects to recieve information from web page.
+  // Create AJAX objects to receive information from web page.
   form_iterator itts = cgi.getElement("type_select");
   form_iterator itname = cgi.getElement("name");
 
+  // Retrieve the actual paramaters
   string type = **itts;
   string name = **itname;
+
   /* Convert name to upper case*/
   transform(name.begin(), name.end(), name.begin(), ::toupper);
 
+  // Lookup the name in the appropriate name map
   if (type.compare("Last")==0) {
     matchList = lastMap.getMatches(name);
   } else if (type.compare("Female")==0){
@@ -63,6 +68,7 @@ int main() {
   /* send back the results */
   cout << "Content-Type: text/plain\n\n";
 
+  /* Results are comma delimited (name, percent, rank, name, ...) for up to 10 names */
   for (int i = 0; i < matchList.size(); i++) {
     cout << matchList.at(i).name << "," <<  matchList.at(i).percent << "," << matchList.at(i).rank;
     if (i<matchList.size()-1)
